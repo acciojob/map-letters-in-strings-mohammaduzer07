c7f6cbaa299aa6788e36aba6f8dc1163aaff7b26
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-02 06:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function mapLetters(word) {
+	let objMap = {};
+	for(let i = 0; i < word.length; i++){
+		let char = word[i];
+		if(objMap.hasOwnProperty(char)){
+			objMap[char].push(i);
+		} else {
+			objMap[char] = i;
+		}
+	}
+	return objMap;
+}
+
+console.log(mapLetters("dodo"))
+console.log(mapLetters("froggy"))
+console.log(mapLetters("grapes"))
